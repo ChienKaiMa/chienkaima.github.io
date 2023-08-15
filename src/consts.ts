7bac6f90bb50057f3c1697d5c6a223eca73815ed
ChienKaiMa <koovakevy@gmail.com>
2023-08-16 01:19:35 +0800

    Replace parts of the template with personal information

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Chien-Kai (Kevin) Ma';
+export const SITE_DESCRIPTION = '2nd year Master student at National Taiwan University';
